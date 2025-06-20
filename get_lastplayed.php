739c1cf9119e4e8a249a4b8f1ccb3f35085dd0cb
romestylez <romestylez@users.noreply.github.com>
2025-06-20 11:30:35 +0200

    Delete get_lastplayed.php

--- get_lastplayed.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: application/json");
-$filename = "clip_history.json";
-if (file_exists($filename)) {
-    echo file_get_contents($filename);
-} else {
-    echo "{}";
-}
